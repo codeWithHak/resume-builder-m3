6caf438884721df6a283f68dfb379df41e94ee66
Huzair Ahmed Khan <huzairahmedkhan@gmail.com>
2024-10-17 10:49:04 +0500

    logging fullname

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+const userData = document.getElementById("user-data");
+const resume = document.getElementById("resume");
+let fullNameInput = document.getElementById("full-name");
+const submit = document.getElementById("submit");
+const fullName = fullNameInput.value;
+function showData() {
+    console.log(fullName);
+}
---
+++ index.ts
@@ -0,0 +1,11 @@
+const userData = document.getElementById("user-data") as HTMLElement
+const resume = document.getElementById("resume") as HTMLElement
+let fullNameInput = document.getElementById("full-name") as HTMLInputElement
+const submit = document.getElementById("submit") as HTMLButtonElement
+const fullName = fullNameInput.value;
+
+
+function showData (){
+    console.log(fullName);
+    
+}
